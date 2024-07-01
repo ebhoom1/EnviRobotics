b9d15416926cc17c04ffd76925d8beae335a4369
Mohamed Fazil M <info.ebhoom@gmail.com>
2024-07-02 04:46:45 +0530

    url edit

--- client/src/utils/apiConfig.js
+++ client/src/utils/apiConfig.js
@@ -1,4 +1,4 @@
-export const API_URL = 'http://43.205.211.237:5555'
+export const API_URL = 'http://ocems.ebhoom.com:5555'
 export const LOCAL_API_URL = 'http://localhost:5555' 
 
 //'http://13.202.11.195:5555'
--- server/app.js
+++ server/app.js
@@ -36,7 +36,7 @@ DB();
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:3001', 'http://43.205.211.237:5555'],
+    origin: ['http://localhost:3000', 'http://localhost:3001', 'http://ocems.ebhoom.com','http://43.205.211.237:5555',],
     credentials: true
 }));
 app.use(cookieParser());
